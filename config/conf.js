0579fcd5f031759dc4b74fe9f06e2ab56f0fb6b1
Ruixin Wu <92225479+ruiXinWu@users.noreply.github.com>
2022-07-19 12:13:26 -0700

    first message

---
+++ config/conf.js
@@ -0,0 +1,16 @@
+if (process.env.NODE_ENV == 'development') { //开发环境
+  module.exports =  {
+    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
+    baseUrl: 'http://localhost:5840'   //java项目接口地址
+  }
+}else if (process.env.NODE_ENV == 'testing') {  //测试环境
+  module.exports = {
+    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
+    baseUrl: 'http://localhost:5840'  //java项目接口地址
+  }
+}else { //默认生产环境
+  module.exports = {
+    title: '领课教育系统(开源版)-打造全行业都适用的在线教育系统',
+    baseUrl: 'http://localhost:5840'   //java项目接口地址
+  }
+}
